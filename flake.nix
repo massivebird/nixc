9cbb0ef9144de019107ee5fe295cb9561737a0e5
massivebird <gdrakemail@gmail.com>
2023-06-18 17:21:40 -0400

    fuck

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "please work please";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-23.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { home-manager, nixpkgs, ... }:
+  let
+    system = "x86_64-linux";
+
+    pkgs = import nixpkgs {
+      inherit system;
+      config = { allowUnfree = true; };
+    };
+
+    lib = nixpkgs.lib;
+  in
+  {
+    nixosConfigurations = {
+      ray = lib.nixosSystem {
+        inherit system;
+
+        modules = [
+          ./system/configuration.nix
+        ];
+      };
+    };
+  };
+}
